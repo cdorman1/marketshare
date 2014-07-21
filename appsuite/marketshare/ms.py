b24c2118244953a3403edc396d2d8944fa211a14
Chris Dorman <cdorman1@users.noreply.github.com>
2014-07-21 13:54:13 -0500

    Update ms.py

--- appsuite/marketshare/ms.py
+++ appsuite/marketshare/ms.py
@@ -1,21 +1,56 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import pyql
-import json
-import pprint
+import sqlalchemy as sa
+from sqlalchemy import exc
 
-def web_scraper(symbol):
 
-    sym_vol = [float(item['Volume']) for item in pyql.lookup(symbol)]
-    ms = [round(1000000 / i * 100, 2) for i in sym_vol]
-    data = {'symbol': [str(item['symbol']) for item in pyql.lookup(symbol)],
-            'Avg_Volume(3m)': [str(item['AverageDailyVolume']) for  item in pyql.lookup(symbol)],
+def pos_query():
+    '''Function for querying DB and returning results'''
+    
+    DB_URI = 'mysql://tclerk:flamengo@cer-emdbl2/'
+    engine = sa.create_engine(DB_URI)
+    engine.begin()
+    try:
+        connection = engine.connect()
+        result = connection.execute("SELECT pm.Symbol, "
+                                    "SUM(pm.Buys + pm.Sells) AS EM_Volume "
+                                    "FROM history.pos_matrix AS pm "
+                                    "WHERE pm.ProdType='Equity' "
+                                    "AND pm.TradeDate=CURDATE() "
+                                    "GROUP BY pm.Symbol;"
+                                    )
+    except exc.SQLAlchemyError.message as sa_message:
+        print sa_message
+        pass
+        
+    return result  
+ 
+
+def web_scraper():
+
+    data = pos_query()
+    symbol = []
+    em_vol = []
+    for k in data:
+        new_d = dict(k)
+        symbol.append(new_d['Symbol'])
+        em_vol.append(new_d['EM_Volume'])
+        
+    print symbol[0:200], em_vol[0:200]
+        
+    sym_vol = [int(item['Volume']) for item in pyql.lookup(symbol[0:200])]
+    ms = [round(x / y * 100, 2) for x, y in zip(em_vol[0:200], sym_vol)]
+    data = {'Symbol': [str(item['symbol']) for item in pyql.lookup(symbol[0:200])],
+            'Avg_Volume(3m)': [str(item['AverageDailyVolume']) for  item in
+            pyql.lookup(symbol[0:200])],
             'Volume': sym_vol,
-            'EM_Volume': ['1000000'] * len(symbol),
+            'EM_Volume': [vol for vol in em_vol[0:200]],
             'Market_Share': map(float, ms)
             }
-    headings = ['symbol', 'Avg_Volume(3m)', 'Volume', 'EM_Volume', 'Market_Share']
+    print data['Market_Share']
+    headings = ['Symbol', 'Avg_Volume(3m)', 'Volume',
+                'EM_Volume', 'Market_Share']
     columns = [data[heading] for heading in headings]
     max_len = len(max(columns, key=len))
     for col in columns:
@@ -25,8 +60,12 @@ def web_scraper(symbol):
     data1 = dict(rows=rows, headings=headings)
     return data1
 
+def main():
+    try:
+        web_scraper()
+    except Exception as e:
+        print "ERROR!!!\n", e
+        
 
 if __name__ == '__main__':
-#    symbol = ['aapl', 'tsla', 'spy']
-    web_scraper(symbol)
-
+    web_scraper()
