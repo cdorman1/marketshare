ea084124578391daf4e24bb092a7e1722118dd18
Chris Dorman <cdorman1@users.noreply.github.com>
2014-07-21 17:03:56 -0500

    Update ms.py

--- appsuite/marketshare/ms.py
+++ appsuite/marketshare/ms.py
@@ -20,10 +20,10 @@ def pos_query():
                                     "AND pm.TradeDate=CURDATE() "
                                     "GROUP BY pm.Symbol;"
                                     )
-    except exc.SQLAlchemyError.message as sa_message:
-        print sa_message
+    except exc.SQLAlchemyError.message as sa_msg:
+        print sa_msg
         pass
-        
+
     return result  
  
 
@@ -37,27 +37,23 @@ def web_scraper():
         symbol.append(new_d['Symbol'])
         em_vol.append(new_d['EM_Volume'])
         
-    print symbol[0:200], em_vol[0:200]
-        
-    sym_vol = [int(item['Volume']) for item in pyql.lookup(symbol[0:200])]
-    ms = [round(x / y * 100, 2) for x, y in zip(em_vol[0:200], sym_vol)]
-    data = {'Symbol': [str(item['symbol']) for item in pyql.lookup(symbol[0:200])],
-            'Avg_Volume(3m)': [str(item['AverageDailyVolume']) for  item in
-            pyql.lookup(symbol[0:200])],
+    sym_vol = [int(item['Volume']) for item in pyql.lookup(symbol[450:])]
+    mkt_shr = [round(x / y * 100, 2) for x, y in zip(em_vol[450:], sym_vol)]
+    data = {'Symbol': [str(item['symbol']) for item in 
+            pyql.lookup(symbol[450:])],
+            'Avg_Volume(3m)': [str(item['AverageDailyVolume']) for item in
+            pyql.lookup(symbol[450:])],
             'Volume': sym_vol,
-            'EM_Volume': [vol for vol in em_vol[0:200]],
-            'Market_Share': map(float, ms)
+            'EM_Volume': [vol for vol in em_vol[450:]],
+            'Market_Share': map(float, mkt_shr)
             }
-    print data['Market_Share']
     headings = ['Symbol', 'Avg_Volume(3m)', 'Volume',
                 'EM_Volume', 'Market_Share']
     columns = [data[heading] for heading in headings]
     max_len = len(max(columns, key=len))
-    for col in columns:
-        col += [None,] * (max_len - len(col))
-
     rows = [[col[i] for col in columns] for i in range(max_len)]
     data1 = dict(rows=rows, headings=headings)
+    print data1
     return data1
 
 def main():
